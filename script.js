628bb976de791a7e964591f42465b0063a6cf919
Dimithri8 <dimithridananjaya924@gmail.com>
2025-05-26 11:52:22 +0530

    Add function logic for game and round score calculation

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 0) {
-    return "Rock";
+    return "rock";
   } else if (randomNumber === 1) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Sissors";
+    return "scissors";
   }
 }
 
 function getHumanChoice() {
-  let userInput = prompt("Type 'rock', 'paper', or 'sissors'");
-  return userInput;
+  let userInput = prompt("Type 'rock', 'paper', or 'scissors'");
+  return userInput.toLowerCase();
 }
-console.log(getHumanChoice());
-console.log(getComputerChoice());
+// 2, 3, 5, 1, 4,
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  } else if (humanChoice == "rock" && computerChoice === "scissors") {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  } else {
+    console.log(`It's a draw! ${humanChoice} doesn't beat ${computerChoice}`);
+    humanScore = humanScore;
+    computerScore = computerScore;
+  }
+  let roundResults = `You score ${humanScore} and your opponent score ${computerScore}`;
+  return roundResults;
+}
 
-// Generate a random number from 0 - 2. DONE
-// IF the number is 0 return rock. DONE
-// IF ELSE the number is 1 return paper. DONE
-// ELSE return sissors (ideally the number would be only 2). DONE
+console.log(playRound(getHumanChoice(), getComputerChoice()));
